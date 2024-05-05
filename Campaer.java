8eedf8d15cc70c65bb0148dd2566342dac265e40
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-05-05 13:18:29 +0530

    Add files via upload

---
+++ Campaer.java
@@ -0,0 +1,32 @@
+class Campaer
+{
+
+public static void main(String args[]){
+
+
+int x =2;
+int y=3; 
+int z=4;
+
+
+if(x>y && x>z){
+
+
+System.out.println("x is bigger then other 2 num")
+
+
+}
+else if(y>x && y>z){
+
+System.out.println("y is bigger then other 2 num")
+
+}
+
+else {
+System.out.println("z is bigger then other 2 num")
+
+}
+
+}
+
+}
