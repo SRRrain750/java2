4054897bc1e215b14887d2daabcb9823985b96a8
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-05-05 22:23:23 +0530

    Add files via upload

---
+++ Class_obj.java
@@ -0,0 +1,21 @@
+class Class_obj
+{
+
+ //public by default
+
+ String person="tayban";
+ int age =22;
+  
+public static void main(String arg[]){
+
+  person p1=new person();//creating a objet by using new() classname
+
+  System.out.println(p1.name);
+  System.out.println(p1.age);
+
+  }
+}
+
+ 
+
+  
