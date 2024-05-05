8c5202993d79e319592238c7af2962e5e6284e2f
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-05-05 12:48:26 +0530

    Add files via upload

---
+++ postiveanadnegtive.java
@@ -0,0 +1,23 @@
+class postiveandnegtive
+{
+
+
+public static void main(String args[]){
+
+
+int x=4;
+
+
+if(x>0)
+{
+
+System.out.println(x+" is positive");
+}
+
+else{
+
+System.out.println(x+" is nagtive");
+
+}
+}
+}
