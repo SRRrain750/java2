c2c4d2cdedd2efd12899988cd8b4f2c6cbdce3e9
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-05-24 21:43:03 +0530

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,29 @@
+class person
+{
+int DHT()
+{
+int Temp=34;
+return(temp);
+}
+
+}
+class Child extends person{
+int DHT{
+int humidity=50;
+return(humidity);
+}
+}
+
+
+class Human
+{
+public static void main(String args[])
+{
+person p1=new person();
+Child p2=new Child();
+
+
+System.out.println("humanity is="+humantiy);
+System.out.println("temp is="+temp);
+}
+}
